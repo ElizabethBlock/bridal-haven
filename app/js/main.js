f0725dfbbc47750e563d42cdf074b915ce217912
Lisa <Lizaoma0@gmail.com>
2025-06-06 11:47:22 +0300

    added product

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.accessories__slider', {
+   slidesPerView: 3,
+   spaceBetween: 40,
+   loop: true,
+   navigation: {
+     nextEl: '.arrow-slider-next',
+     prevEl: '.arrow-slider-prev',
+   },
+ });
+ 
